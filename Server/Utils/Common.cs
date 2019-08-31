f0c45ca4144740fe106b66e6d58ae451517c9058
Paweł Okrasa <pawel.jacek.okrasa@intel.com>
2019-08-31 16:24:30 +0200

    Utils

---
+++ Server/Utils/Common.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace Server.Utils
+{
+    public static class Common
+    {
+        public static void TryCatchAction<TException>(Action action, Action<TException> excpetionHandler, ILogger logger = null) where TException : Exception
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch(TException ex)
+            {
+                if (logger != null) logger.LogError(ex.ToString());
+                excpetionHandler.Invoke(ex);
+            }
+        }
+    }
+}
