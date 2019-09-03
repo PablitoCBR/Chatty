2a08d3a74bda3ef5275c1e210c1a7ce8469ecada
Paweł Okrasa <pawel.jacek.okrasa@intel.com>
2019-09-03 15:06:25 +0200

    Service registration

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -5,6 +5,8 @@ using Server.Host;
 using Server.Host.Builder;
 using Server.Host.Builder.Interfaces;
 using Server.Host.HostAbstractions.Interfaces;
+using Server.Host.Listener;
+using Server.Host.Listener.Interfaces;
 
 namespace Server
 {
@@ -14,7 +16,7 @@ namespace Server
 
         public override void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddSingleton<IListenerFabric, ListenerFabric>();
             services.AddScoped<IServerHostBuilder, ServerHostBuilder>();
             services.AddTransient<IServerHostSetup, ServerHost>();
 
